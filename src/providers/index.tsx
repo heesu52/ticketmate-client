d80ff5806afef01efea4fe7083d642f8d4bea702
imironjin <cjfwls9806@gmail.com>
2025-02-16 22:45:31 +0900

    feat: ios 용 스플래시 스크린 추가 #9

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,9 +1,15 @@
 import { type ReactNode } from 'react';
 
+import HeadProvider from '@/providers/head-provider';
 import ReactQueryProviders from '@/providers/react-query-provider';
 
 const Provider = ({ children }: { children: ReactNode }) => {
-  return <ReactQueryProviders>{children}</ReactQueryProviders>;
+  return (
+    <>
+      <HeadProvider />
+      <ReactQueryProviders>{children}</ReactQueryProviders>;
+    </>
+  );
 };
 
 export default Provider;

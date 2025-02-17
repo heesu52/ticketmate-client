77e4c583b76dfdf6bfe1238bce96dfd1e4df2bcf
Heesu Jang <heesu52@naver.com>
2025-02-17 21:28:29 +0900

    refactor: prettier 적용 #26

--- src/providers/react-query-provider.tsx
+++ src/providers/react-query-provider.tsx
@@ -1,11 +1,17 @@
-"use client";
+'use client';
 
 import { useState, type ReactNode } from 'react';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export default function ReactQueryProviders({ children }: { children: ReactNode }) {
+export default function ReactQueryProviders({
+  children,
+}: {
+  children: ReactNode;
+}) {
   const [queryClient] = useState(() => new QueryClient());
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 }

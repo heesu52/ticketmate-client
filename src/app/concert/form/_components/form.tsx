14bf576196829397a7a93b3c9108de6d87f62479
Heesu Jang <heesu52@naver.com>
2025-02-23 22:35:23 +0900

    design: 대리인 매칭폼 퍼블리싱 #31

---
+++ src/app/concert/form/_components/form.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import MinusIcon from '@/assets/icons/minus.svg';
+import PlusIcon from '@/assets/icons/plus.svg';
+import PositiveButton from '@/shared/components/button/positive-button/positive-button';
+import AppBarSetter from '@/shared/components/header/app-bar/app-bar-setter';
+
+import styles from './form.module.scss';
+
+export default function Form() {
+  return (
+    <>
+      <AppBarSetter title="신청 양식" />
+      <div className={styles.container}>
+        <div className={styles.form_container}>
+          <span className={styles.span}>회차</span>
+          <div className={styles.select}>2024/12/09(월) (2회차)</div>
+        </div>
+        <div className={styles.form_container}>
+          <span className={styles.span}>매수</span>
+          <div className={styles.select}>1매</div>
+        </div>
+        <div className={styles.form_container}>
+          <span className={styles.span}>희망구역</span>
+
+          <div className={styles.input_container}>
+            <span className={styles.text}>1순위</span>
+            <input
+              placeholder="구역명"
+              id="seat"
+              className={styles.input_seat}
+            />
+            <input
+              placeholder="가격"
+              id="price"
+              className={styles.input_price}
+            />
+            <MinusIcon width={16} height={16} fill={'var(--gray-4)'} />
+          </div>
+          <div className={styles.input_container}>
+            <span className={styles.text}>2순위</span>
+            <input
+              placeholder="구역명"
+              id="seat"
+              className={styles.input_seat}
+            />
+            <input
+              placeholder="가격"
+              id="price"
+              className={styles.input_price}
+            />
+            <MinusIcon width={16} height={16} fill={'var(--gray-4)'} />
+          </div>
+          <div className={styles.input_container}>
+            <span className={styles.text}>3순위</span>
+            <input
+              placeholder="구역명"
+              id="seat"
+              className={styles.input_seat}
+            />
+            <input
+              placeholder="가격"
+              id="price"
+              className={styles.input_price}
+            />
+            <PlusIcon width={16} height={16} fill={'var(--gray-4)'} />
+          </div>
+        </div>
+        <div className={styles.form_container}>
+          <span className={styles.span}>요청사항</span>
+          <textarea
+            className={styles.textarea}
+            placeholder="자유롭게 입력해주세요."
+          />
+        </div>
+        <PositiveButton label="신청하기" type="button" size="large" />
+      </div>
+    </>
+  );
+}

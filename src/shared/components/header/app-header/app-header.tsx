f1c587deb663a83dddda9c88a2453bbf84acec75
imironjin <cjfwls9806@gmail.com>
2025-02-24 20:57:24 +0900

    feat: 메인페이지 검색창 삭제 및 app-header에 검색 버튼 추가 #35

--- src/shared/components/header/app-header/app-header.tsx
+++ src/shared/components/header/app-header/app-header.tsx
@@ -1,5 +1,6 @@
 import TicketMateLogoIcon from '@/assets/icons/main_logo.svg';
 import NotificationIcon from '@/assets/icons/notification.svg';
+import SearchIcon from '@/assets/icons/search.svg';
 
 import styles from './app-header.module.scss';
 
@@ -14,11 +15,14 @@ const AppHeader = () => {
           aria-hidden="true"
         />
       </h1>
-      <button className={styles.button} aria-label="알림">
-        <span className={styles.icon}>
+      <div className={styles.button_group}>
+        <button className={styles.button} aria-label="검색">
+          <SearchIcon width={24} height={24} fill="var(--gray-5)" />
+        </button>
+        <button className={styles.button} aria-label="알림">
           <NotificationIcon width={24} height={24} fill="var(--gray-5)" />
-        </span>
-      </button>
+        </button>
+      </div>
     </div>
   );
 };

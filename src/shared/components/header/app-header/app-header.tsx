12e8e90b193db292e8a60e68ea8bedd7fbc15375
imironjin <cjfwls9806@gmail.com>
2025-03-31 15:54:23 +0900

    design: app header 디자인 수정 #48

--- src/shared/components/header/app-header/app-header.tsx
+++ src/shared/components/header/app-header/app-header.tsx
@@ -1,5 +1,4 @@
 import TicketMateLogoIcon from '@/assets/icons/main_logo.svg';
-import NotificationIcon from '@/assets/icons/notification.svg';
 import SearchIcon from '@/assets/icons/search.svg';
 
 import styles from './app-header.module.scss';
@@ -7,20 +6,17 @@ import styles from './app-header.module.scss';
 const AppHeader = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title} aria-label="티켓메이트 로고">
-        <TicketMateLogoIcon
-          width={102}
-          height={16}
-          role="img"
-          aria-hidden="true"
-        />
-      </h1>
+      <TicketMateLogoIcon
+        width={102}
+        height={22}
+        role="img"
+        aria-hidden="true"
+        aria-label="티켓메이트 로고"
+      />
+
       <div className={styles.button_group}>
         <button className={styles.button} aria-label="검색">
-          <SearchIcon width={24} height={24} fill="var(--gray-5)" />
-        </button>
-        <button className={styles.button} aria-label="알림">
-          <NotificationIcon width={24} height={24} fill="var(--gray-5)" />
+          <SearchIcon width={20} height={20} fill="var(--textColor-main)" />
         </button>
       </div>
     </div>

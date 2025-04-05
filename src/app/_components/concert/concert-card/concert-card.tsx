f18071e5d77e5155170cd4778481632371ff50dc
Heesu Jang <heesu52@naver.com>
2025-04-06 00:16:04 +0900

    refactor: 메인페이지 css 변경사항 적용 #46

--- src/app/_components/concert/concert-card/concert-card.tsx
+++ src/app/_components/concert/concert-card/concert-card.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link';
 
+import { LocationOnIcon } from '@/assets/icons';
+import Badge from '@/shared/components/badge/badge';
+
 import styles from './concert-card.module.scss';
 
 interface ConcertItem {
   title: string;
   date: string;
-  agent: string;
+  place: string;
   img: string;
 }
 
@@ -18,23 +21,29 @@ const ConcertCard = ({ concertItem }: ConcertCardProps) => {
     <>
       <Link href={'concert/:id'}>
         <div className={styles.container}>
-          <div className={styles.concert_img}>
-            {/* 추후 next의 Image 로 변경 예정 */}
-            <img
-              src={concertItem.img}
-              alt={concertItem.title}
-              width={104}
-              height={139}
-            />
+          <div className={styles.tag}>
+            <Badge type="d-day">선예매까지 D-12</Badge>
+            <Badge type="d-day">일반예매까지 D-12</Badge>
           </div>
-          <div className={styles.concert_info}>
-            <div className={styles.description}>
-              <span className={styles.date}>{concertItem.date}</span>
-              <span className={styles.title}>{concertItem.title}</span>
+          <div className={styles.card_container}>
+            <div className={styles.concert_img}>
+              {/* 추후 next의 Image 로 변경 예정 */}
+              <img
+                src={concertItem.img}
+                alt={concertItem.title}
+                width={104}
+                height={139}
+              />
             </div>
-            <div className={styles.agent}>
-              <span className={styles.agent_label}>대리인 수</span>
-              <span className={styles.agent_count}>{concertItem.agent}</span>
+            <div className={styles.concert_info}>
+              <div className={styles.description}>
+                <span className={styles.date}>{concertItem.date}</span>
+                <span className={styles.title}>{concertItem.title}</span>
+              </div>
+              <div className={styles.place}>
+                <LocationOnIcon width={16} height={16} />
+                <span className={styles.location}>{concertItem.place}</span>
+              </div>
             </div>
           </div>
         </div>
--- src/app/_components/concert/concert-list/concert-list.tsx
+++ src/app/_components/concert/concert-list/concert-list.tsx
@@ -3,57 +3,70 @@
 import React from 'react';
 
 import ConcertCard from '@/app/_components/concert/concert-card/concert-card';
+import { FilterListIcon } from '@/assets/icons';
 
+import ConcertDropdown from '../concert-dropdown';
 import styles from './concert-list.module.scss';
 
 const concert = [
   {
-    title: '2터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
-    date: '24년 11월 25일(월) 18:00',
-    agent: '10명',
+    title: '터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
+    date: '2025.01.25 ~ 2025.02.01 ',
+    place: '블루스퀘어 마스터카드홀',
     img: 'https://placehold.co/400x600',
   },
   {
-    title: '3터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
-    date: '24년 11월 25일(월) 18:00',
-    agent: '30명',
+    title: '터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
+    date: '2025.01.25 ~ 2025.02.01 ',
+    place: '블루스퀘어 마스터카드홀',
     img: 'https://placehold.co/400x600',
   },
   {
-    title: '4터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
-    date: '24년 11월 25일(월) 18:00',
-    agent: '5명',
+    title: '터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
+    date: '2025.01.25 ~ 2025.02.01 ',
+    place: '블루스퀘어 마스터카드홀',
     img: 'https://placehold.co/400x600',
   },
   {
-    title: '5터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
-    date: '24년 11월 25일(월) 18:00',
-    agent: '1명',
+    title: '터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
+    date: '2025.01.25 ~ 2025.02.01 ',
+    place: '블루스퀘어 마스터카드홀',
     img: 'https://placehold.co/400x600',
   },
   {
-    title: '6터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
-    date: '24년 11월 25일(월) 18:00',
-    agent: '100명',
+    title: '터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
+    date: '2025.01.25 ~ 2025.02.01 ',
+    place: '블루스퀘어 마스터카드홀',
     img: 'https://placehold.co/400x600',
   },
   {
-    title: '7터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
-    date: '24년 11월 25일(월) 18:00',
-    agent: '5명',
+    title: '터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
+    date: '2025.01.25 ~ 2025.02.01 ',
+    place: '블루스퀘어 마스터카드홀',
     img: 'https://placehold.co/400x600',
   },
   {
-    title: '8터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
-    date: '24년 11월 25일(월) 18:00',
-    agent: '1명',
+    title: '터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
+    date: '2025.01.25 ~ 2025.02.01 ',
+    place: '블루스퀘어 마스터카드홀',
     img: 'https://placehold.co/400x600',
   },
   {
-    title: '9터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
-    date: '24년 11월 25일(월) 18:00',
-    agent: '100명',
+    title: '터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’',
+    date: '2025.01.25 ~ 2025.02.01 ',
+    place: '블루스퀘어 마스터카드홀',
     img: 'https://placehold.co/400x600',
+  },
+];
+
+const dropdownList = [
+  {
+    value: 'popularity',
+    label: '인기순',
+  },
+  {
+    value: 'latest',
+    label: '날짜순',
   },
 ];
 
@@ -63,8 +76,14 @@ const ConcertList = () => {
       <div className={styles.container}>
         {/* 리스트 */}
         <div className={styles.concert_container}>
-          <span className={styles.title}>신청가능 공연</span>
+          <div className={styles.title_contanier}>
+            <span className={styles.title}>신청가능 공연</span>
 
+            <div className={styles.dropdown_container}>
+              <ConcertDropdown dropdownList={dropdownList} />
+              <FilterListIcon width={20} height={20} />
+            </div>
+          </div>
           {concert.map((concertItem) => (
             <ConcertCard concertItem={concertItem} key={concertItem.title} />
           ))}

684c8914c8c782d205a51d896f9b452df9c2bbf1
imironjin <cjfwls9806@gmail.com>
2025-04-16 08:17:39 +0900

    feat: 동기 작동 추가 및 테스트용 코드 추가 #53

--- src/shared/services/instance.ts
+++ src/shared/services/instance.ts
@@ -36,13 +36,18 @@ const instance = httpClient({
       init: NonNullable<Parameters<typeof fetch>[1]>,
     ): Promise<T> {
       if (!response.ok) {
+        console.log(response);
         if (response.status === 401) {
           const newAccessToken = await getAccessTokenFromServer();
 
+          console.log(response, newAccessToken);
+
           if (newAccessToken) {
+            const accessToken = await getCookie('accessToken');
+
             const retryHeaders = {
               ...init.headers,
-              Authorization: `Bearer ${newAccessToken}`,
+              Authorization: `Bearer ${accessToken}`,
             };
 
             const retryResponse = await fetch(input, {
@@ -57,16 +62,16 @@ const instance = httpClient({
               const retryErrorText = await retryResponse.text();
               throw new Error(`Retry failed: ${retryErrorText}`);
             }
-          }
-
-          // 재발급 실패 시 로그인으로
-          if (
-            window.confirm(
-              '로그인이 필요한 페이지입니다. 로그인 창으로 이동하시겠습니까?',
-            )
-          ) {
-            window.location.href = '/auth/sign-in';
-            return Promise.reject('Redirecting to login');
+          } else {
+            // 재발급 실패 시 로그인으로
+            if (
+              window.confirm(
+                '로그인이 필요한 페이지입니다. 로그인 창으로 이동하시겠습니까?',
+              )
+            ) {
+              window.location.href = '/auth/sign-in';
+              return Promise.reject('Redirecting to login');
+            }
           }
         }
 
--- src/shared/utils/auth.ts
+++ src/shared/utils/auth.ts
@@ -1,16 +1,20 @@
 import { getCookie } from 'cookies-next';
 
 export const getAccessTokenFromServer = async () => {
-  const refreshToken = getCookie('refreshToken');
+  const refreshToken = await getCookie('refreshToken');
 
   if (!refreshToken) return false;
+
+  console.log('getRefresh', refreshToken);
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/reissue`, {
     method: 'POST',
     credentials: 'include',
   });
 
+  console.log('reissue', res);
+
   if (!res.ok || res.status !== 200) return false;
 
-  return getCookie('accessToken');
+  return res.ok;
 };

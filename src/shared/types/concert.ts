68ca97972379a7bbfa1f8b839b6bf5de748b8168
imironjin <cjfwls9806@gmail.com>
2025-04-15 14:46:45 +0900

    feat: 공연 및 pagination 관련 타입 추가 #53

---
+++ src/shared/types/concert.ts
@@ -0,0 +1,34 @@
+type ConcertType =
+  | 'CONCERT'
+  | 'MUSICAL'
+  | 'SPORTS'
+  | 'CLASSIC'
+  | 'EXHIBITIONS'
+  | 'OPERA'
+  | 'ETC';
+
+type TicketReservationSite =
+  | 'INTERPARK_TICKET'
+  | 'YES24_TICKET'
+  | 'TICKEK_LINK'
+  | 'MELON_TICKET'
+  | 'COUPANG_PLAY'
+  | 'ETC';
+
+interface Concert {
+  concertId: string; // 공연 ID
+  concertName: string; // 공연 제목
+  concertHallName: string; // 공연장 이름
+  concertType: string; // 공연 카테고리
+  ticketReservationSite: string; // 예매처 사이트
+  ticketPreOpenDate: string; // 선예매 시작일
+  preOpenBankTransfer: boolean; // 선예매 계좌이체 가능 여부
+  ticketGeneralOpenDate: string; // 일반 예매 시작일
+  generalOpenBankTransfer: boolean; // 일반 예매 계좌이체 가능 여부
+  startDate: string; // 공연 시작일
+  endDate: string; // 공연 종료일
+  concertThumbnailUrl: string; // 공연 썸네일 URL
+  seatingChartUrl: string; // 좌석 배치도 URL
+}
+
+export type { ConcertType, TicketReservationSite, Concert };

97cb9c92a5de77b05596dec343d87932aae4b837
Heesu Jang <heesu52@naver.com>
2025-04-28 23:33:10 +0900

    design: 신청폼에서 제출 버튼 클릭 시 예약완료 모달 구현 #59

---
+++ src/app/concert/form/_shared/components/form-modal/form-modal.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import React from 'react';
+
+import { useRouter } from 'next/navigation';
+
+import Button from '@/shared/components/button/functional-button/functional-button';
+import CustomModal from '@/shared/components/modal/custom-modal';
+
+interface FormeModalProps {
+  title: string;
+  message: string;
+  onConfirm?: () => Promise<void> | void;
+  onCancel?: () => void;
+}
+
+const FormModal = ({
+  title,
+  message,
+  onConfirm,
+  onCancel,
+}: FormeModalProps) => {
+  const router = useRouter(); // useRouter 훅을 사용하여 라우팅 처리
+
+  const handleConfirm = async () => {
+    if (onConfirm) await onConfirm();
+    router.push(`/concert/:id`);
+  };
+
+  const handleCancel = () => {
+    if (onCancel) onCancel();
+    router.push('/history');
+  };
+
+  return (
+    <CustomModal>
+      <CustomModal.Title>{title}</CustomModal.Title>
+      <CustomModal.Description>{message}</CustomModal.Description>
+      <CustomModal.Action>
+        <Button size="medium" variant="border" onClick={handleConfirm}>
+          확인했어요
+        </Button>
+        <Button size="medium" variant="fill" onClick={handleCancel}>
+          신청내역 보러가기
+        </Button>
+      </CustomModal.Action>
+    </CustomModal>
+  );
+};
+
+export default FormModal;

642b628cf51a2044d56a344c24390b21e9916612
heesu52 <huisujang80@gmail.com>
2025-05-31 18:07:25 +0900

    feat: concertinfo 상세정보 조회 시 type 변경 #70

--- src/shared/types/concert.ts
+++ src/shared/types/concert.ts
@@ -15,6 +15,20 @@ type TicketReservationSite =
   | 'COUPANG_PLAY'
   | 'ETC';
 
+interface ConcertDateInfo {
+  performanceDate: string; // 공연 일자
+  session: number; // 세션 번호
+}
+
+type TicketOpenType = 'PRE_OPEN' | 'GENERAL_OPEN';
+
+interface TicketOpenDateInfo {
+  openDate: string; // 오픈 날짜
+  requestMaxCount: number; // 최대 예매 매수
+  isBankTransfer: boolean; // 계좌이체 가능 여부
+  ticketOpenType: TicketOpenType; // 오픈 타입
+}
+
 interface Concert {
   concertId: string; // 공연 ID
   concertName: string; // 공연 제목
@@ -27,12 +41,8 @@ interface Concert {
   generalOpenBankTransfer: boolean; // 일반 예매 계좌이체 가능 여부
 
   //상세공연 정보 조회 시 선예매, 일반예매에 대한 응답값
-  preOpenDate: string; // 선예매 시작일
-  preOpenIsBankTransfer: boolean; // 선예매 계좌이체 가능 여부
-  preOpenRequestMaxCount: number; // 선예매 최대 예매 매수
-  generalOpenDate: string; // 일반 예매 시작일
-  generalOpenIsBankTransfer: boolean; // 일반 예매 계좌이체 가능 여부
-  generalOpenRequestMaxCount: number; // 일반예매 최대 예매 매수
+  ticketOpenDateInfoResponses: TicketOpenDateInfo[]; // 예매 관련 정보
+  concertDateInfoResponseList: ConcertDateInfo[]; // 공연 날짜 정보
 
   startDate: string; // 공연 시작일
   endDate: string; // 공연 종료일

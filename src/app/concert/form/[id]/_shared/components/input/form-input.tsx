91eb5209e6982595defd8ff0fe4f4293ac053c55
heesu52 <huisujang80@gmail.com>
2025-06-03 01:23:29 +0900

    feat: 신청폼 요청 페이로드에 맞게 type 및 input 수정 #71

--- src/app/concert/form/[id]/_shared/components/input/form-input.tsx
+++ src/app/concert/form/[id]/_shared/components/input/form-input.tsx
@@ -4,36 +4,52 @@ import { MinusIcon, PlusIcon, HelpCircleIcon } from '@/assets/icons';
 import Input from '@/shared/components/input/input';
 
 import styles from './form-input.module.scss';
-import { FormData, InputItem, dateList, countList } from './form-input.type';
+import { FormData, HopeArea } from './form-input.type';
 import FormSelect from '../select/form-select';
 
 interface FormInputProps {
   value: FormData;
   onChange: (data: FormData) => void;
+  dateList: { value: string; label: string }[];
+  countList: { value: string; label: string }[];
 }
 
-export default function FormInput({ value, onChange }: FormInputProps) {
-  const [inputs, setInputs] = useState<InputItem[]>(
-    value?.inputs || [{ id: 1, seat: '', price: '' }],
+export default function FormInput({
+  value,
+  onChange,
+  dateList,
+  countList,
+}: FormInputProps) {
+  const [hopeAreaList, setHopeAreaList] = useState<HopeArea[]>(
+    value?.hopeAreaList || [{ id: 1, location: '', price: '' }],
   );
-  const [date, setDate] = useState<string>(value?.date || '');
-  const [count, setCount] = useState<string>(value?.count || '');
-  const [note, setNote] = useState<string>(value?.note || '');
+  const [performanceDate, setPerformanceDate] = useState<string>(
+    value?.performanceDate || '',
+  );
+  const [requestCount, setRequestCount] = useState<string>(
+    value?.requestCount || '',
+  );
+  const [requestDetails, setRequestDetails] = useState<string>(
+    value?.requestDetails || '',
+  );
 
   useEffect(() => {
-    onChange({ inputs, date, count, note });
-  }, [inputs, date, count, note]);
+    onChange({ performanceDate, requestCount, hopeAreaList, requestDetails });
+  }, [performanceDate, requestCount, hopeAreaList, requestDetails]);
 
   const addInput = () => {
-    setInputs((prev) => [...prev, { id: Date.now(), seat: '', price: '' }]);
+    setHopeAreaList((prev) => [
+      ...prev,
+      { id: Date.now(), location: '', price: '' },
+    ]);
   };
 
   const removeInput = (id: number) => {
-    setInputs((prev) => prev.filter((input) => input.id !== id));
+    setHopeAreaList((prev) => prev.filter((input) => input.id !== id));
   };
 
   const handleInputChange = (id: number, field: string, value: string) => {
-    setInputs((prev) =>
+    setHopeAreaList((prev) =>
       prev.map((input) =>
         input.id === id ? { ...input, [field]: value } : input,
       ),
@@ -46,14 +62,22 @@ export default function FormInput({ value, onChange }: FormInputProps) {
         <span className={styles.span}>
           회차<span className={styles.asterisk}>*</span>
         </span>
-        <FormSelect selectList={dateList} value={date} onChange={setDate} />
+        <FormSelect
+          selectList={dateList}
+          value={performanceDate}
+          onChange={setPerformanceDate}
+        />
       </div>
 
       <div className={styles.form_container}>
         <span className={styles.span}>
           매수<span className={styles.asterisk}>*</span>
         </span>
-        <FormSelect selectList={countList} value={count} onChange={setCount} />
+        <FormSelect
+          selectList={countList}
+          value={requestCount}
+          onChange={setRequestCount}
+        />
       </div>
 
       <div className={styles.form_container}>
@@ -65,7 +89,7 @@ export default function FormInput({ value, onChange }: FormInputProps) {
           </div>
         </div>
 
-        {inputs.map((input, index) => (
+        {hopeAreaList.map((input, index) => (
           <div key={input.id} className={styles.input_container}>
             <span className={styles.text}>{`${index + 1}순위`}</span>
             {/* 구역명 입력 */}
@@ -73,9 +97,9 @@ export default function FormInput({ value, onChange }: FormInputProps) {
               label="구역명"
               placeholder="구역명"
               id={`seat-${input.id}`}
-              value={input.seat}
+              value={input.location}
               onChange={(e) =>
-                handleInputChange(input.id, 'seat', e.target.value)
+                handleInputChange(input.id, 'location', e.target.value)
               }
             />
 
@@ -89,7 +113,7 @@ export default function FormInput({ value, onChange }: FormInputProps) {
                 handleInputChange(input.id, 'price', e.target.value)
               }
             />
-            {index === inputs.length - 1 ? (
+            {index === hopeAreaList.length - 1 ? (
               <PlusIcon
                 width={16}
                 height={16}
@@ -115,8 +139,8 @@ export default function FormInput({ value, onChange }: FormInputProps) {
         <textarea
           className={styles.textarea}
           placeholder="자유롭게 입력해주세요."
-          value={note}
-          onChange={(e) => setNote(e.target.value)}
+          value={requestDetails}
+          onChange={(e) => setRequestDetails(e.target.value)}
         />
       </div>
     </div>
--- src/app/concert/form/[id]/_shared/components/input/form-input.type.ts
+++ src/app/concert/form/[id]/_shared/components/input/form-input.type.ts
@@ -1,24 +1,12 @@
-export type InputItem = {
-  seat: string;
-  price: string;
+export type HopeArea = {
   id: number;
+  location: string;
+  price: string; // 나중에 전송 시 Number로 변환
 };
 
 export type FormData = {
-  inputs: InputItem[];
-  date: string;
-  count: string;
-  note: string;
+  performanceDate: string; // 기존: date
+  requestCount: string; // 기존: count
+  hopeAreaList: HopeArea[]; // 기존: inputs
+  requestDetails: string; // 기존: note
 };
-
-export const dateList = [
-  { value: '2024-12-01', label: '2024/12/01(금) (1회차)' },
-  { value: '2024-12-02', label: '2024/12/02(토) (2회차)' },
-  { value: '2024-12-03', label: '2024/12/03(일) (3회차)' },
-];
-
-export const countList = [
-  { value: '1', label: '1매' },
-  { value: '2', label: '2매' },
-  { value: '3', label: '3매' },
-];

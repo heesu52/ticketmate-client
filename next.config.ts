636a265938237910f2345d38592a82a8edc2a800
heesu52 <huisujang80@gmail.com>
2025-06-20 17:34:42 +0900

    feat: strictmode false로 변경 #81

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },

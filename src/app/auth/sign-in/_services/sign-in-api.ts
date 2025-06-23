fb465be464881b7de38dd08662be21df485d7ac8
heesu52 <huisujang80@gmail.com>
2025-06-23 15:04:54 +0900

    refactor: sign-in-apit 기존코드로 변경 #81

--- src/app/auth/sign-in/_services/sign-in-api.ts
+++ src/app/auth/sign-in/_services/sign-in-api.ts
@@ -1,7 +1,7 @@
 import { SignInAPIBody } from '@/app/auth/sign-in/_services/sign-in-api.type';
 import instance from '@/shared/services/instance';
 
-const postSignInAPI = async (data: SignInAPIBody) => {
+const postSignInAPI = (data: SignInAPIBody) => {
   return instance(
     `/auth/sign-in?username=${data.username}&password=${data.password}`,
     {

4e68e6a82653971c1d5efc9eedaa29806ccb8398
imironjin <cjfwls9806@gmail.com>
2025-06-27 16:08:42 +0900

    feat: 채팅방 상세페이지 API 연동 및 디자인 수정 #89

---
+++ src/app/chat/[id]/_shared/components/chat-message-list/chat-message-list.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+
+import classNames from 'classnames/bind';
+import dayjs from 'dayjs';
+import Image from 'next/image';
+
+import type { ChatMessage as ChatMessageType } from '@/app/chat/[id]/_shared/services/type';
+
+import styles from './chat-message-list.module.scss';
+
+interface ChatMessageListProps {
+  messages: ChatMessageType[];
+}
+
+const cn = classNames.bind(styles);
+
+/** 같은 사람이 보낸 메시지인지 확인 */
+const isSameSender = (a: ChatMessageType, b: ChatMessageType) =>
+  a?.senderId === b?.senderId && a?.mine === b?.mine;
+
+/** 같은 시간에 보낸 메시지인지 확인 */
+const isSameMinute = (a: ChatMessageType, b: ChatMessageType) =>
+  dayjs(a.sendDate).format('YYYY-MM-DD HH:mm') ===
+  dayjs(b.sendDate).format('YYYY-MM-DD HH:mm');
+
+/** 같은 날 보낸 메시지인지 확인 */
+const isSameDate = (a: ChatMessageType, b: ChatMessageType) =>
+  dayjs(a.sendDate).format('YYYY-MM-DD') ===
+  dayjs(b.sendDate).format('YYYY-MM-DD');
+
+const ChatMessageList = ({ messages }: ChatMessageListProps) => {
+  return (
+    <div className={styles.container}>
+      {messages.map((msgItem, idx) => {
+        /** 이전 메시지 */
+        const prev = messages[idx - 1];
+        /** 다음 메시지 */
+        const next = messages[idx + 1];
+
+        /** 보낸 사람이 내 혹은 상대방인지 확인 */
+        const isMine = msgItem.mine;
+        /** 이전 메시지와 같은 사람이 보낸 메시지인지 확인 */
+        const isSameGroupWithPrev =
+          prev && isSameSender(msgItem, prev) && isSameMinute(msgItem, prev);
+        /** 다음 메시지와 같은 사람이 보낸 메시지인지 확인 */
+        const isSameGroupWithNext =
+          next && isSameSender(msgItem, next) && isSameMinute(msgItem, next);
+
+        /** 그룹핑된 메세지의 첫 번째 메시지인지 확인 */
+        const isFirstOfGroup = !isSameGroupWithPrev;
+        /** 그룹핑된 메세지의 마지막 메시지인지 확인 */
+        const isLastOfGroup = !isSameGroupWithNext;
+
+        /** 시간 구분 선 표시 여부 */
+        const showDateDivider = !prev || !isSameDate(msgItem, prev);
+        /** 시간 구분 선 표시 날짜 */
+        const formattedDate = dayjs(msgItem.sendDate).format(
+          'YYYY년 MM월 DD일',
+        );
+
+        /** 그룹 간격 */
+        const groupSpacing = isFirstOfGroup ? { marginTop: '24px' } : {};
+
+        return (
+          <React.Fragment key={msgItem.messageId}>
+            {showDateDivider && (
+              <div className={styles.date_divider}>{formattedDate}</div>
+            )}
+            <div
+              className={cn(
+                'list_container',
+                isMine ? 'my_message' : 'other_message',
+              )}
+              style={groupSpacing}
+            >
+              {!isMine && (
+                <div className={styles.profile_image_wrapper}>
+                  <Image
+                    src={msgItem.profileUrl}
+                    alt="profile"
+                    width={36}
+                    height={36}
+                    className={cn(
+                      styles.profile_image,
+                      isFirstOfGroup ? '' : styles.hidden,
+                    )}
+                  />
+                </div>
+              )}
+
+              <div className={styles.message_container}>
+                <div className={styles.message_bubble}>
+                  <div className={styles.message_content}>
+                    {msgItem.message}
+                  </div>
+                </div>
+
+                {isLastOfGroup && (
+                  <div className={styles.message_time}>
+                    {dayjs(msgItem.sendDate).format('A h:mm')}
+                  </div>
+                )}
+              </div>
+            </div>
+          </React.Fragment>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ChatMessageList;
--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,130 +1,23 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { use } from 'react';
 
 import AppBarSetter from '@/app/_components/layout/header/app-bar/app-bar-setter';
 import ChatHeader from '@/app/chat/[id]/_shared/components/chat-header/chat-header';
 import ChatInput from '@/app/chat/[id]/_shared/components/chat-input/chat-input';
+import useGetChatDetail from '@/app/chat/[id]/_shared/services/query';
 
-import ChatMessage from './_shared/components/chat-message/chat-message';
+import ChatMessageList from './_shared/components/chat-message-list/chat-message-list';
 import styles from './page.module.scss';
 
-// 임시 메시지 데이터
-const mockMessages = [
-  {
-    id: 1,
-    content: '안녕하세요',
-    timestamp: '14:30',
-    isMyMessage: true,
-  },
-  {
-    id: 2,
-    content: '터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’ 대리 모집받았습니다.',
-    timestamp: '14:31',
-    isMyMessage: true,
-  },
-  {
-    id: 3,
-    content: '연속된 채팅창 ',
-    timestamp: '14:31',
-    isMyMessage: true,
-  },
-  {
-    id: 4,
-    content: '안녕하세요.',
-    timestamp: '12:51',
-    isMyMessage: false,
-  },
-  {
-    id: 5,
-    content: '어떤 부분이 궁금해서 연락주셨나요?? 편하게 말씀해주세요.',
-    timestamp: '12:51',
-    isMyMessage: false,
-  },
-  {
-    id: 6,
-    content: '안녕하세요',
-    timestamp: '12:51',
-    isMyMessage: true,
-  },
-  {
-    id: 7,
-    content: '안녕하세요',
-    timestamp: '12:51',
-    isMyMessage: true,
-  },
-  {
-    id: 8,
-    content: '안녕하세요',
-    timestamp: '12:51',
-    isMyMessage: true,
-  },
-  {
-    id: 9,
-    content: '안녕하세요',
-    timestamp: '12:51',
-    isMyMessage: true,
-  },
-  {
-    id: 10,
-    content: '안녕하세요',
-    timestamp: '12:51',
-    isMyMessage: true,
-  },
-  {
-    id: 11,
-    content: '안녕하세요',
-    timestamp: '12:51',
-    isMyMessage: true,
-  },
-  {
-    id: 12,
-    content: '안녕하세요',
-    timestamp: '12:51',
-    isMyMessage: true,
-  },
-  {
-    id: 13,
-    content: '안녕하세요',
-    timestamp: '12:51',
-    isMyMessage: true,
-  },
-];
+interface ChatDetailPageProps {
+  params: Promise<{ id: string }>;
+}
 
-const ChatDetailPage = () => {
-  const [messages] = useState(mockMessages);
+const ChatDetailPage = ({ params }: ChatDetailPageProps) => {
+  const { id } = use(params);
 
-  const shouldShowProfile = (currentIndex: number) => {
-    const currentMessage = messages[currentIndex];
-    if (currentMessage.isMyMessage) {
-      return false;
-    }
-
-    // 이전 메시지가 없거나, 내 메시지인 경우에만 프로필 표시
-    if (currentIndex === 0 || messages[currentIndex - 1].isMyMessage) {
-      return true;
-    }
-
-    return false;
-  };
-
-  const shouldShowTimestamp = (currentIndex: number) => {
-    const currentMessage = messages[currentIndex];
-    const nextMessage = messages[currentIndex + 1];
-
-    if (!nextMessage) {
-      return true;
-    }
-
-    if (
-      nextMessage.isMyMessage !== currentMessage.isMyMessage ||
-      nextMessage.timestamp !== currentMessage.timestamp
-    ) {
-      return true;
-    }
-
-    return false;
-  };
+  const { data: messages } = useGetChatDetail({ chatRoomId: id });
 
   return (
     <>
@@ -133,17 +26,8 @@ const ChatDetailPage = () => {
         <div className={styles.header_container}>
           <ChatHeader />
         </div>
-
         <div className={styles.messages_container}>
-          {messages.map((message, index) => (
-            <ChatMessage
-              key={message.id}
-              message={message}
-              showProfile={shouldShowProfile(index)}
-              showTimestamp={shouldShowTimestamp(index)}
-              profileImage="https://picsum.photos/200/300"
-            />
-          ))}
+          <ChatMessageList messages={messages ?? []} />
         </div>
 
         <div className={styles.input_container}>

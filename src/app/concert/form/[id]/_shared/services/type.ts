6835c077ce041159f734f71ae24cecf90a5d6e72
heesu52 <huisujang80@gmail.com>
2025-06-27 12:25:09 +0900

    feat: ticketype 공용 type사용으로 변경 #88

--- src/app/concert/form/[id]/_shared/services/type.ts
+++ src/app/concert/form/[id]/_shared/services/type.ts
@@ -1,4 +1,4 @@
-import { Form } from '@/shared/types';
+import { Form, TicketOpenType } from '@/shared/types';
 
 interface HopeArea {
   priority: number; // 우선순위
@@ -16,8 +16,6 @@ interface ApplicationFormDetail {
 interface ApplicationFormRequest {
   applicationFormDetailRequestList: ApplicationFormDetail[]; // 신청 공연 회차 목록
 }
-
-type TicketOpenType = 'PRE_OPEN' | 'GENERAL_OPEN';
 
 interface CreateConcertFormRequest extends ApplicationFormRequest {
   agentId: string; // 대리인 PK
@@ -45,48 +43,3 @@ export type {
   GetFormDetailRequest,
   GetFormDetailResponse,
 };
-
-//   import { Form } from '@/shared/types';
-
-// interface HopeArea {
-//   priority: number; // 우선순위
-//   location: string; // 구역명
-//   price: number; // 가격
-// }
-
-// interface ApplicationFormDetail{
-//   performanceDate: string; // 공연 일자
-//   requestCount: number; // 요청한 티켓 수
-//   hopeAreaList?: HopeArea[]; // 희망 구역 리스트 (선택)
-//   requestDetails?: string; // 요청 사항 (선택)
-// }
-
-// type TicketOpenType = 'PRE_OPEN' | 'GENERAL_OPEN';
-
-// interface CreateConcertFormRequest {
-//   agentId: string; // 대리인 PK
-//   concertId: string; // 콘서트 PK
-//   applicationFormDetailRequestList: ApplicationFormDetail[]; // 신청 공연 회차 목록 [최소 1개 이상 필수]
-
-//   ticketOpenType: TicketOpenType; // 선예매 여부
-// }
-
-// interface PatchConcertFormRequest {
-//   applicationFormId: string;
-//   applicationFormEditRequest: {
-//     applicationFormDetailRequestList: ApplicationFormDetail[];
-//   }; // 신청 공연 회차 목록 [최소 1개 이상 필수]
-// }
-
-// interface GetFormDetailRequest {
-//   applicationFormId: string; // 폼ID [필수]
-// }
-
-// type GetFormDetailResponse = Form;
-
-// export type {
-//   CreateConcertFormRequest,
-//   PatchConcertFormRequest,
-//   GetFormDetailRequest,
-//   GetFormDetailResponse,
-// };

d541701a7ddfce518116dc11637fb1c88aa06284
imironjin <cjfwls9806@gmail.com>
2025-07-17 22:16:01 +0900

    feat: 채팅방 더보기 버튼 추가 #89

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -5,6 +5,7 @@ import React, { use } from 'react';
 import AppBarSetter from '@/app/_components/layout/header/app-bar/app-bar-setter';
 import ChatHeader from '@/app/chat/[id]/_shared/components/chat-header/chat-header';
 import ChatInput from '@/app/chat/[id]/_shared/components/chat-input/chat-input';
+import { MoreIcon } from '@/assets/icons';
 
 import ChatMessageList from './_shared/components/chat-message-list/chat-message-list';
 import styles from './page.module.scss';
@@ -18,7 +19,19 @@ const ChatDetailPage = ({ params }: ChatDetailPageProps) => {
 
   return (
     <>
-      <AppBarSetter title="의뢰인" />
+      <AppBarSetter
+        title="의뢰인"
+        action={
+          <button type="button" aria-label="더보기 버튼">
+            <MoreIcon
+              width={20}
+              height={20}
+              fill={`var(--textColor-main)`}
+              aria-hidden="true"
+            />
+          </button>
+        }
+      />
       <div className={styles.container}>
         <div className={styles.header_container}>
           <ChatHeader />

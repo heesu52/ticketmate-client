26a227bdd5e843db31b582a0e64878ec73c97e61
heesu52 <huisujang80@gmail.com>
2025-07-20 00:05:26 +0900

    feat: 날짜 지난 공연 필터링 하는 코드 삭제 #98

--- src/app/_components/concert/concert-list/concert-list.tsx
+++ src/app/_components/concert/concert-list/concert-list.tsx
@@ -6,7 +6,6 @@ import ConcertCard from '@/app/_components/concert/concert-card/concert-card';
 import { useGetConcertList } from '@/app/_shared/services/query';
 import { GetConcertListRequest } from '@/app/_shared/services/type';
 import { useIntersectionObserver } from '@/shared/hooks/use-intersection-observer';
-import { calculateDday } from '@/shared/utils/dates';
 
 import ConcertSelect from '../concert-select';
 import styles from './concert-list.module.scss';
@@ -38,37 +37,23 @@ const ConcertList = () => {
   };
 
   const concertList = data?.content;
-  //tickettype이 선예매만 존재하고 D+ 로 시작하는 공연은 제외하고 리스트에 출력
-  const filteredConcertList = concertList?.filter((concert) => {
-    if (concert.ticketPreOpenDate && !concert.ticketGeneralOpenDate) {
-      const dday = calculateDday(concert.ticketPreOpenDate);
-
-      if (dday.startsWith('D+')) {
-        return false;
-      }
-      return true;
-    }
-    return true;
-  });
 
   return (
     <>
       <div className={styles.container}>
         <div className={styles.concert_container}>
-          <div className={styles.title_contanier}>
+          <div className={styles.title_container}>
             <span className={styles.title}>신청가능 공연</span>
 
             <div className={styles.select_container}>
               <ConcertSelect onSelect={handleSelect} />
             </div>
           </div>
-          {filteredConcertList?.map((concertItem, index) => (
+          {concertList?.map((concertItem, index) => (
             <div
               key={concertItem.concertId}
               ref={
-                index === filteredConcertList.length - 1
-                  ? lastElementRef
-                  : undefined
+                index === concertList.length - 1 ? lastElementRef : undefined
               }
               className={styles.card_wrapper}
             >

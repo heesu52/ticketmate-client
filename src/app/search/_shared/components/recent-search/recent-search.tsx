f181c6d955dc1bf3538926b79117ad686e30e839
heesu52 <huisujang80@gmail.com>
2025-09-04 16:45:11 +0900

    feat: 동일한 search폴더 추가해서 새로고침 후 같은 화면 보이도록 구현 #115

---
+++ src/app/search/_shared/components/recent-search/recent-search.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import React from 'react';
+
+import {
+  useDeleteRecentSearchMutation,
+  useRecentSearchQuery,
+} from '@/app/@modal/(.)search/_shared/services/query';
+
+import styles from './recent-search.module.scss';
+
+interface RecentSearchProps {
+  isLoggedIn: boolean;
+  localRecent: string[];
+  setLocalRecent: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+export default function RecentSearch({
+  isLoggedIn,
+  localRecent,
+  setLocalRecent,
+}: RecentSearchProps) {
+  const { data: recentSearch } = useRecentSearchQuery(isLoggedIn);
+  const { mutate } = useDeleteRecentSearchMutation();
+
+  // 로그인 여부에 따른 최근검색어 삭제
+  const handleDeleteAll = () => {
+    if (isLoggedIn) {
+      mutate();
+    } else {
+      localStorage.removeItem('recentSearches');
+      setLocalRecent([]);
+    }
+  };
+
+  // 로그인 여부에 따라 데이터를 응답값 or localstorage
+  const recentList = isLoggedIn ? recentSearch : localRecent;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.span_container}>
+        <span className={styles.title}>최근 검색어</span>
+        <button className={styles.delete} onClick={handleDeleteAll}>
+          전체삭제
+        </button>
+      </div>
+      <div className={styles.tag_container}>
+        {recentList?.map((tag, index) => (
+          <div key={index} className={styles.tag}>
+            {tag}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/app/search/_shared/components/search-tab/search-tab-manager.tsx
@@ -0,0 +1,141 @@
+'use client';
+
+import { useMemo, useState } from 'react';
+
+import {
+  useGetAgentSearchQuery,
+  useGetConcertSearchQuery,
+} from '@/app/@modal/(.)search/_shared/services/query';
+import { GetSearchRequest } from '@/app/@modal/(.)search/_shared/services/type';
+import ConcertCard from '@/app/_components/concert/concert-card/concert-card';
+import UserCard from '@/app/concert/[id]/_shared/components/user-card/user-card';
+import TabButton from '@/shared/components/ui/tab/tab';
+import { TabItem } from '@/shared/components/ui/tab/tab.type';
+import { AgentInfo, Concert } from '@/shared/types';
+
+import styles from './search-tab-manager.module.scss';
+
+interface SearchTabManagerProps {
+  searchRequest: GetSearchRequest | null;
+}
+
+export default function SearchTabManager({
+  searchRequest,
+}: SearchTabManagerProps) {
+  // 탭의 기본값은 CONCERT로 설정 -> 탭 클릭 시 상태 값 변경
+  const [active, setActive] = useState<'CONCERT' | 'AGENT'>(
+    searchRequest?.searchType ?? 'CONCERT',
+  );
+
+  // 탭 변경 시 호출되는 api의 요청값
+  const keyword = searchRequest?.keyword ?? '';
+  const request = useMemo<GetSearchRequest>(
+    () => ({
+      keyword,
+      searchType: active,
+    }),
+    [keyword, active],
+  );
+  // enabled 조건: 키워드가 있고, 해당 탭이 활성화되어 있을 때만
+  const enabledConcert = !!keyword && active === 'CONCERT';
+  const enabledAgent = !!keyword && active === 'AGENT';
+
+  // 공연 검색 조회 (처음 검색을 제외하고 사용)
+  const { data: concertRes, isFetching: isFetchingConcert } =
+    useGetConcertSearchQuery(request, {
+      enabled: enabledConcert,
+    });
+
+  // 대리인 검색 조회 (현재 서버 500 에러 발생 -> 백엔드에서 수정 중)
+  const { data: agentRes, isFetching: isFetchingAgent } =
+    useGetAgentSearchQuery(request, {
+      enabled: enabledAgent,
+    });
+
+  // 공연/대리인 정보 (타입이 달라서 별도로 지정)
+  const concertList: Concert[] = concertRes?.content ?? [];
+  const agentList: AgentInfo[] = agentRes?.content ?? [];
+  const concertCount =
+    active === 'CONCERT'
+      ? (concertRes?.concertCount ?? 0)
+      : (agentRes?.concertCount ?? 0);
+
+  const agentCount =
+    active === 'AGENT'
+      ? (agentRes?.agentCount ?? 0)
+      : (concertRes?.agentCount ?? 0);
+
+  // const isFetching = active === 'CONCERT' ? isFetchingConcert : isFetchingAgent;
+
+  const tabs: TabItem[] = useMemo(
+    () => [
+      { value: 'CONCERT', label: `공연 ${concertCount}`, content: <div /> },
+      { value: 'AGENT', label: `대리인 ${agentCount}`, content: <div /> },
+    ],
+    [concertCount, agentCount],
+  );
+
+  return (
+    <div className={styles.container}>
+      {/* 탭 */}
+      <TabButton
+        items={tabs}
+        value={active}
+        onValueChange={(v) => setActive(v as 'CONCERT' | 'AGENT')}
+      />
+
+      {/* 검색결과 리스트 */}
+      {/* 검색을 안했다면 컴포넌트 노출x */}
+      {searchRequest && (
+        <>
+          {active === 'CONCERT' ? (
+            <>
+              {/* 로딩을 확인하기 위해 넣었는데 추후 icon으로 수정필요
+              {isFetching && (
+                <div className={styles.skeleton}>로딩중/div>
+              )} */}
+              {concertList.length === 0 && (
+                <div className={styles.empty_container}>
+                  <span>검색결과 없음</span>
+                </div>
+              )}
+              {concertList.length > 0 && (
+                <div className={styles.list_container}>
+                  {concertList.map((concertItem) => (
+                    <div
+                      key={concertItem.concertId}
+                      className={styles.card_wrapper}
+                    >
+                      <ConcertCard concertItem={concertItem} />
+                    </div>
+                  ))}
+                </div>
+              )}
+            </>
+          ) : (
+            <>
+              {/* 로딩을 확인하기 위해 넣었는데 추후 icon으로 수정필요
+              {isFetching && (
+                <div className={styles.skeleton}>로딩중/div>
+              )} */}
+              {agentList.length === 0 && (
+                <div className={styles.empty_container}>
+                  <span>검색결과 없음</span>
+                </div>
+              )}
+              {agentList.length > 0 && (
+                <div className={styles.list_container}>
+                  {agentList.map((agent) => (
+                    <div key={agent.agentId} className={styles.card_wrapper}>
+                      <UserCard user={agent} />
+                    </div>
+                  ))}
+                </div>
+              )}
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+}

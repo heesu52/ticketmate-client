a891582e38c0fe5d64820de3d5aafb16fd614db7
imironjin <cjfwls9806@gmail.com>
2025-09-26 09:08:41 +0900

    feat(마이페이지): setting-item 컴포넌트 추가 #127

---
+++ src/shared/components/features/my/setting-item/setting-item.tsx
@@ -0,0 +1,78 @@
+import Link from 'next/link';
+
+import { ArrowRightIcon } from '@/assets/icons';
+
+import styles from './setting-item.module.scss';
+
+import type { SettingItem as SettingItemType } from './setting-item.type';
+
+type SettingItemProps = SettingItemType;
+
+const SettingItem = ({
+  title,
+  description,
+  trailing,
+  href,
+  onClick,
+  disabled = false,
+  type = 'link',
+  trailingIcon,
+}: SettingItemProps) => {
+  const Trailing = (
+    <div className={styles.item_trailing_container}>
+      {trailing && <span className={styles.item_trailing}>{trailing}</span>}
+      {type === 'link' &&
+        (trailingIcon ?? (
+          <ArrowRightIcon width={12} height={12} fill="var(--grayscale-500)" />
+        ))}
+    </div>
+  );
+
+  // 링크 항목
+  if (type === 'link') {
+    return (
+      <Link href={href ?? ''} className={styles.item} aria-disabled={disabled}>
+        <div className={styles.title_container}>
+          <span className={styles.item_title}>{title}</span>
+          {description && (
+            <span className={styles.item_desc}>{description}</span>
+          )}
+        </div>
+        {Trailing}
+      </Link>
+    );
+  }
+
+  // 클릭 액션(버튼) 항목
+  if (type === 'action') {
+    return (
+      <button
+        type="button"
+        className={styles.item}
+        onClick={onClick}
+        disabled={disabled}
+      >
+        <div className={styles.title_container}>
+          <span className={styles.item_title}>{title}</span>
+          {description && (
+            <span className={styles.item_desc}>{description}</span>
+          )}
+        </div>
+        {Trailing}
+      </button>
+    );
+  }
+
+  // 텍스트만 표시
+  return (
+    <div className={styles.item} aria-disabled={disabled}>
+      <div className={styles.title_container}>
+        <span className={styles.item_title}>{title}</span>
+        {description && <span className={styles.item_desc}>{description}</span>}
+      </div>
+      {Trailing}
+    </div>
+  );
+};
+
+export default SettingItem;
---
+++ src/shared/components/features/my/setting-item/setting-item.type.ts
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+
+export interface SettingItem {
+  title: string;
+  description?: string;
+  trailing?: ReactNode;
+  href?: string; // link일 때 사용
+  onClick?: () => void; // action일 때 사용
+  disabled?: boolean;
+  type?: 'link' | 'text' | 'action';
+  trailingIcon?: ReactNode; // 우측 아이콘 교체용
+}

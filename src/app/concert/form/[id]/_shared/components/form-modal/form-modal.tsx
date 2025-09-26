7f8705525a218031b0c16c939f9e63ebb1624e5d
heesu52 <huisujang80@gmail.com>
2025-09-26 11:33:49 +0900

    feat: form-modal 공통컴포넌트로 수정 #128

--- src/app/concert/form/[id]/_shared/components/form-modal/form-modal.tsx
+++ src/app/concert/form/[id]/_shared/components/form-modal/form-modal.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 import { useRouter } from 'next/navigation';
 
-import Button from '@/shared/components/button/functional-button/functional-button';
-import CustomModal from '@/shared/components/modal/custom-modal';
+import Button from '@/shared/components/ui/button/button';
+import Modal from '@/shared/components/ui/modal/modal';
 
 interface FormeModalProps {
   title: string;
@@ -35,18 +35,18 @@ const FormModal = ({
   };
 
   return (
-    <CustomModal>
-      <CustomModal.Title>{title}</CustomModal.Title>
-      <CustomModal.Description>{message}</CustomModal.Description>
-      <CustomModal.Action>
-        <Button size="medium" variant="border" onClick={handleConfirm}>
-          확인했어요
+    <Modal>
+      <Modal.Title>{title}</Modal.Title>
+      <Modal.Description>{message}</Modal.Description>
+      <Modal.Action>
+        <Button variant="fill" color="gray" onClick={handleConfirm}>
+          다음에
         </Button>
-        <Button size="medium" variant="fill" onClick={handleCheck}>
-          신청내역 보러가기
+        <Button variant="fill" onClick={handleCheck}>
+          신청하기
         </Button>
-      </CustomModal.Action>
-    </CustomModal>
+      </Modal.Action>
+    </Modal>
   );
 };
 

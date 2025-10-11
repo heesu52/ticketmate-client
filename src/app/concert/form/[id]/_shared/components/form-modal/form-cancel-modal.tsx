dabbf43466726b6b0c5f4fe037d4b8104d260123
heesu52 <huisujang80@gmail.com>
2025-10-12 03:16:42 +0900

    refactor: form-modal 파일이름 및 위치변경 #135

---
+++ src/app/concert/form/[id]/_shared/components/form-modal/form-cancel-modal.tsx
@@ -0,0 +1,35 @@
+import { usePatchFormCancel } from '@/app/history/_shared/services/mutation';
+import ModalTemplate from '@/shared/components/ui/modal/modal-template/modal-template';
+import { ModalControl } from '@/shared/components/ui/modal/modal.type';
+
+type FormCancelModalProps = {
+  applicationFormId: string;
+} & ModalControl<unknown>;
+
+const FormCancelModal = ({
+  onResolve,
+  onReject,
+  applicationFormId,
+}: FormCancelModalProps) => {
+  const { mutate } = usePatchFormCancel();
+
+  const handleSecondButtonClick = () => {
+    mutate(applicationFormId, {
+      onSuccess: () => onResolve?.(true),
+      onError: () => onReject?.(),
+    });
+  };
+
+  return (
+    <ModalTemplate
+      title="신청을 취소하시겠습니까?"
+      description="신청했던 내역은 과거 신청내역에서 확인 가능합니다."
+      firstButtonLabel="다음에"
+      secondButtonLabel="취소하기"
+      onFirstButtonClick={() => onReject?.()}
+      onSecondButtonClick={handleSecondButtonClick}
+    />
+  );
+};
+
+export default FormCancelModal;

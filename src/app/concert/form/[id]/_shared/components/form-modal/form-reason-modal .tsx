35f78b9c9c303b8b1ead837acdf77da69daf8708
heesu52 <huisujang80@gmail.com>
2025-10-17 13:39:17 +0900

    fix: 모달 안뜨는 에러 해결 #135

--- src/app/concert/form/[id]/_shared/components/form-modal/form-reason-modal .tsx
+++ src/app/concert/form/[id]/_shared/components/form-modal/form-reason-modal .tsx
@@ -1,68 +1,64 @@
-// 'use client';
+'use client';
 
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-// import { useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
-// import { getRejectionReason } from '@/app/history/_shared/services/api';
-// import ModalTemplate from '@/shared/components/ui/modal/modal-template/modal-template';
-// import { ModalTemplateType } from '@/shared/components/ui/modal/modal-template/modal-template.type';
-// import { ModalControl } from '@/shared/components/ui/modal/modal.type';
-// import { toastify } from '@/shared/components/ui/toast/toastify';
-// import { APPLICATION_REJECTED_LABEL_MAP } from '@/shared/constants/type-mapping';
-// import { ApplicationRejectedType } from '@/shared/types';
+import { getRejectionReason } from '@/app/history/_shared/services/api';
+import ModalTemplate from '@/shared/components/ui/modal/modal-template/modal-template';
+import { ModalControl } from '@/shared/components/ui/modal/modal.type';
+import { toastify } from '@/shared/components/ui/toast/toastify';
+import { APPLICATION_REJECTED_LABEL_MAP } from '@/shared/constants/type-mapping';
+import { ApplicationRejectedType } from '@/shared/types';
 
-// interface FormReasonModalProps extends ModalControl<unknown> {
-//   applicationFormId: string;
-// }
+interface FormReasonModalProps extends ModalControl<unknown> {
+  applicationFormId: string;
+}
 
-// interface GetRejectionReasonResponse {
-//   applicationFormRejectedType: ApplicationRejectedType;
-//   otherMemo: string;
-// }
+interface GetRejectionReasonResponse {
+  applicationFormRejectedType: ApplicationRejectedType;
+  otherMemo: string;
+}
 
-// const FormReasonModal = ({
-//   onResolve,
-//   onReject,
-//   applicationFormId,
-// }: FormReasonModalProps) => {
-//   const { data, isError } = useQuery<GetRejectionReasonResponse>({
-//     queryKey: ['rejectionReason', applicationFormId],
-//     queryFn: () => getRejectionReason(applicationFormId),
-//   });
+const FormReasonModal = ({
+  onReject,
+  applicationFormId,
+}: FormReasonModalProps) => {
+  const [mappedMessage, setMappedMessage] = useState<string>('');
 
-//   useEffect(() => {
-//     if (data) {
-//       const mappedMessage =
-//         APPLICATION_REJECTED_LABEL_MAP[data.applicationFormRejectedType];
-//       const c = data.otherMemo;
-//     } else if (isError) {
-//       toastify({
-//         variant: 'error',
-//         description: '거절 사유 조회를 실패했습니다.',
-//       });
-//     }
-//   }, [data, isError]);
+  const { data, isError } = useQuery<GetRejectionReasonResponse>({
+    queryKey: ['rejectionReason', applicationFormId],
+    queryFn: () => getRejectionReason(applicationFormId),
+  });
 
-//   const handleFirstButtonClick = () => {
-//     onReject?.();
-//   };
+  useEffect(() => {
+    if (data) {
+      const message =
+        APPLICATION_REJECTED_LABEL_MAP[data.applicationFormRejectedType] ??
+        '거절 사유를 불러올 수 없음';
+      setMappedMessage(message);
+    } else if (isError) {
+      toastify({
+        variant: 'error',
+        description: '거절 사유 조회를 실패했습니다.',
+      });
+    }
+  }, [data, isError]);
 
-//   const handleSecondButtonClick = () => {
-//     onResolve?.(true);
-//   };
+  const handleFirstButtonClick = () => {
+    onReject?.();
+  };
 
-//   return (
-//     <ModalTemplate
-//       title="대리인 닉네임의 거절사유"
-//       description={mappedMessage}
-//       children={mappedMessage}
-//       firstButtonLabel="확인했어요"
-//       secondButtonLabel="신청하기"
-//       onFirstButtonClick={handleFirstButtonClick}
-//       onSecondButtonClick={handleSecondButtonClick}
-//     />
-//   );
-// };
+  return (
+    <ModalTemplate
+      title="대리인 닉네임의 거절사유"
+      description={`작성한 신청양식을 통해 동일한 대리인에게 다시 티켓팅을 의뢰할 수 있습니다.`}
+      firstButtonLabel="확인했어요"
+      onFirstButtonClick={handleFirstButtonClick}
+    >
+      {mappedMessage && <p>{mappedMessage}</p>}
+    </ModalTemplate>
+  );
+};
 
-// export default FormReasonModal;
+export default FormReasonModal;
--- src/app/concert/form/[id]/view/page.tsx
+++ src/app/concert/form/[id]/view/page.tsx
@@ -5,7 +5,8 @@ import { useRouter } from 'next/navigation';
 
 import FormInfo from '@/app/concert/form/[id]/_shared/components/form-info/form-info';
 import FormCancelModal from '@/app/concert/form/[id]/_shared/components/form-modal/form-cancel-modal';
-// import FormReasonModal from '@/app/concert/form/[id]/_shared/components/form-modal/form-reason-modal ';
+import FormConfirmModal from '@/app/concert/form/[id]/_shared/components/form-modal/form-confirm-modal';
+import FormReasonModal from '@/app/concert/form/[id]/_shared/components/form-modal/form-reason-modal ';
 import FormTabManager from '@/app/concert/form/[id]/_shared/components/form-tab/form-tab-manager';
 import { useGetFormDetail } from '@/app/concert/form/[id]/_shared/services/query';
 import PageFrame from '@/shared/components/layout/page-frame/page-frame';
@@ -30,6 +31,7 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
   const concertItem = formItem.concertInfoResponse;
 
   const type = searchParamsProps.get('type') as TicketOpenType;
+  const status = state?.applicationFormStatus;
 
   // 에러 발생 시 토스트 알림
   const handleError = (message: string) => {
@@ -37,8 +39,13 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
     console.error(message);
   };
 
+  // clientId & pending: 신청취소
+  // agentId & pending : 거절 or 수락
+  // rejected: 거절사유
+  // cancel,canceld-inprocess : 신청
+
   // 공연 신청 취소 모달 (의뢰인용)
-  const handleOpenModal = async () => {
+  const handleOpenCancelModal = async () => {
     try {
       const result = await open('form-cancel-modal', FormCancelModal, {
         applicationFormId,
@@ -59,10 +66,77 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
     }
   };
 
-  // // 신청서 거절 사유 확인 모달(의뢰인)
-  // const handleOpenModal = () => {
-  //   open('form-reason-modal', FormReasonModal, { applicationFormId });
-  // };
+  // 신청서 거절 사유 확인 모달 (의뢰인용)
+  const handleOpenReasonModal = () => {
+    open('form-reason-modal', FormReasonModal, { applicationFormId });
+  };
+
+  // 신청서 재요청 확인 모달 (의뢰인용)
+  const handleOpenConfirmModal = async () => {
+    try {
+      const result = await open('form-confirm-modal', FormConfirmModal);
+
+      if (result) {
+        toastify({
+          variant: 'success',
+          description: '공연 의뢰에 성공했습니다.',
+        });
+        router.push('/history');
+      }
+    } catch (error) {
+      toastify({
+        variant: 'error',
+        description: '공연 의뢰에 실패했습니다.',
+      });
+    }
+  };
+
+  /*
+  //신청서 거절 모달 (대리인용)
+  const handleOpenRejectedModal = async () => {
+    try {
+      const result = await open('form-rejected-modal', FormRejectedModal);
+
+      if (result) {
+        toastify({
+          variant: 'success',
+          description: '신청이 정상적으로 거절되었습니다',
+        });
+        router.push('/history');
+      }
+    } catch (error) {
+      toastify({
+        variant: 'error',
+        description: '신청 거절에 실패했습니다.',
+      });
+    }
+  };
+  */
+
+  const handleOpenModal = () => {
+    if (!status) return;
+
+    //memberId에 따라서 peding일 때는 모달이 달라져야하는데 일단은 대리인 화면을 기준으로 구현
+    if (status === 'PENDING') {
+      handleOpenCancelModal();
+      /*
+      if (memberId === 'client' && status === 'PENDING') {
+        handleOpenCancelModal();
+      } else if (memberId === 'agent' && status === 'PENDING') {
+        handleOpenRejectedModal();
+      */
+    } else if (status === 'REJECTED') {
+      handleOpenReasonModal();
+    } else if (status === 'CANCELED' || status === 'CANCELED_IN_PROCESS') {
+      handleOpenConfirmModal();
+    } else {
+      toastify({
+        variant: 'error',
+        description: '해당 상태에서는 모달을 열 수 없습니다.',
+      });
+    }
+  };
+
   return (
     <PageFrame
       appBar={{
@@ -87,7 +161,7 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
               ticketOpenType={type}
               applicationFormId={applicationFormId}
               onError={handleError}
-              status={state?.applicationFormStatus} //분기처리를 위해 전달
+              status={status} //분기처리를 위해 전달
             />
           </>
         )}

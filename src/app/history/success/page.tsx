3e78aac52f96006c49a37fe6ff37980f9bb01d66
heesu52 <huisujang80@gmail.com>
2025-11-01 18:22:22 +0900

    feat: membertype에 따른 main_container 노출여부 #149-re

--- src/app/history/success/page.tsx
+++ src/app/history/success/page.tsx
@@ -5,10 +5,13 @@ import SuccessList from '@/app/history/success/_shared/components/success-list';
 import RecentSuccessList from '@/shared/components/features/recent-success/recent-success-list/recent-success-list';
 import PageFrame from '@/shared/components/layout/page-frame/page-frame';
 import Spacer from '@/shared/components/ui/spacer/spacer';
+import { useMember } from '@/shared/context/member-context';
 
 import styles from './page.module.scss';
 
 export default function SuccessPage() {
+  const { member } = useMember();
+
   return (
     <PageFrame
       appBar={{
@@ -18,16 +21,17 @@ export default function SuccessPage() {
       bottomNav={true}
     >
       <div className={styles.container}>
-        <div className={styles.main_container}>
-          <span className={styles.title}>대표 성공내역</span>
-          <span className={styles.subtitle}>
-            대리인으로 인정받은 대표 성공내역입니다.
-          </span>
-          <Spacer size={16} />
-          <SuccessList />
-        </div>
-
-        <Spacer size={40} />
+        {member?.memberType === 'AGENT' && (
+          <div className={styles.main_container}>
+            <span className={styles.title}>대표 성공내역</span>
+            <span className={styles.subtitle}>
+              대리인으로 인정받은 대표 성공내역입니다.
+            </span>
+            <Spacer size={16} />
+            <SuccessList />
+            <Spacer size={40} />
+          </div>
+        )}
 
         {/*  신청내역 날짜별로 */}
         <div className={styles.list_container}>

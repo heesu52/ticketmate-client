c7d03ef6a51b1944cbe4d7d6e3bc0e5e6d53a543
heesu52 <huisujang80@gmail.com>
2025-11-04 09:41:05 +0900

    feat: 한 줄 소개 작성 api 연동 #150

--- src/app/my/application/_shared/services/mutation.ts
+++ src/app/my/application/_shared/services/mutation.ts
@@ -3,7 +3,7 @@ import { useMutation } from '@tanstack/react-query';
 import { createAgentAvailability } from '@/app/my/application/_shared/services/api';
 import { CreateAgentAvailabilityRequest } from '@/app/my/application/_shared/services/type';
 
-export const useCreateAgentAvailability = () => {
+export const useCreateAgentAvailabilityMutation = () => {
   return useMutation({
     mutationFn: (request: CreateAgentAvailabilityRequest) =>
       createAgentAvailability(request),
--- src/app/my/application/bio/page.tsx
+++ src/app/my/application/bio/page.tsx
@@ -1,14 +1,54 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 
+import { useRouter } from 'next/navigation';
+
+import { useCreateAgentAvailabilityMutation } from '@/app/my/application/_shared/services/mutation';
 import PageFrame from '@/shared/components/layout/page-frame/page-frame';
 import Button from '@/shared/components/ui/button/button';
 import Spacer from '@/shared/components/ui/spacer/spacer';
 import Textarea from '@/shared/components/ui/textarea/textarea';
+import { toastify } from '@/shared/components/ui/toast/toastify';
 
 import styles from './page.module.scss';
 
 export default function BioPage() {
+  const [introduction, setIntroduction] = useState('');
+  const router = useRouter();
+  const { mutate } = useCreateAgentAvailabilityMutation();
+
+  // 한 줄 소개 변경
+  const handleIntroductionChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    setIntroduction(e.target.value);
+  };
+
+  // 저장 버튼 클릭
+  const handleSubmit = () => {
+    const requestData = {
+      concertId: 'test',
+      accepting: true,
+      introduction,
+    };
+
+    mutate(requestData, {
+      onSuccess: () => {
+        toastify({
+          variant: 'success',
+          description: '한 줄 소개 작성이 완료됐습니다',
+        });
+        router.push('/my/application');
+      },
+      onError: () => {
+        toastify({
+          variant: 'error',
+          description: '한 줄 소개 작성에 실패됐습니다',
+        });
+      },
+    });
+  };
+
   return (
     <PageFrame
       appBar={{
@@ -22,13 +62,15 @@ export default function BioPage() {
 
         <div className={styles.content}>
           <Textarea
-            // value={introduction}
-            // onChange={handleIntroductionChange}
+            value={introduction}
+            onChange={handleIntroductionChange}
             id="bio"
             placeholder="의뢰인에게 보여줄 공연 한 줄 소개를 작성하세요."
-            style={{ height: '140px' }}
+            style={{ height: '200px' }}
           />
-          <Button variant="fill">저장하기</Button>
+          <Button variant="fill" onClick={handleSubmit}>
+            저장하기
+          </Button>
         </div>
         <Spacer size={20} />
       </div>

6bb5dda05c8b8d1ecf623f7ee9461136753d8278
imironjin <cjfwls9806@gmail.com>
2025-11-09 23:51:13 +0900

    feat: 임시 채팅, 채팅상세 페이지 #143

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -2,10 +2,9 @@
 
 import React, { use } from 'react';
 
-import AppBarSetter from '@/app/_components/layout/header/app-bar/app-bar-setter';
 import ChatHeader from '@/app/chat/[id]/_shared/components/chat-header/chat-header';
 import ChatInput from '@/app/chat/[id]/_shared/components/chat-input/chat-input';
-import { MoreIcon } from '@/assets/icons';
+import PageFrame from '@/shared/components/layout/page-frame/page-frame';
 
 import ChatMessageList from './_shared/components/chat-message-list/chat-message-list';
 import styles from './page.module.scss';
@@ -18,20 +17,13 @@ const ChatDetailPage = ({ params }: ChatDetailPageProps) => {
   const { id: roomId } = use(params);
 
   return (
-    <>
-      <AppBarSetter
-        title="의뢰인"
-        action={
-          <button type="button" aria-label="더보기 버튼">
-            <MoreIcon
-              width={20}
-              height={20}
-              fill={`var(--textColor-main)`}
-              aria-hidden="true"
-            />
-          </button>
-        }
-      />
+    <PageFrame
+      appBar={{
+        title: '상대방 닉네임',
+        showBack: true,
+      }}
+      bottomNav={false}
+    >
       <div className={styles.container}>
         <div className={styles.header_container}>
           <ChatHeader />
@@ -45,7 +37,7 @@ const ChatDetailPage = ({ params }: ChatDetailPageProps) => {
           <ChatInput roomId={roomId} />
         </div>
       </div>
-    </>
+    </PageFrame>
   );
 };
 

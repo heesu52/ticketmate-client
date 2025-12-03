48fbfe2ee164afb9088f8c529316079ae9bb71ed
heesu52 <huisujang80@gmail.com>
2025-12-03 11:32:49 +0900

    fix: sw안에 작성한 firebase config 내용 제거 #162

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,13 +6,7 @@ importScripts(
 );
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyCKlGh__FG-2pje_U8NJ-2o5cFz52FJ8gQ',
-  authDomain: 'ticketmate-bafbb.firebaseapp.com',
-  projectId: 'ticketmate-bafbb',
-  storageBucket: 'ticketmate-bafbb.firebasestorage.app',
   messagingSenderId: '6603871566',
-  appId: '1:6603871566:web:07a3600333e48fb3d87a1f',
-  measurementId: 'G-XLH4GH51JR',
 });
 
 // 백그라운드 알림 처리

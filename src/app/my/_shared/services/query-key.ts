d3725a28e8479d89ecf87e24baf8c4947ce80320
imironjin <cjfwls9806@gmail.com>
2025-12-10 08:54:10 +0900

    chore: 불필요한 파일 정리 #176

--- src/app/my/_shared/services/query-key.ts
+++
@@ -1,5 +0,0 @@
-const queryKey = {
-  getMember: () => ['getMember'] as const,
-};
-
-export default queryKey;
--- src/shared/components/modal/modal-constants.ts
+++
@@ -1,6 +0,0 @@
-export const MODAL_ID = {
-  CANCEL_MODAL: 'cancel-modal',
-  REASON_MODAL: 'reason-modal',
-  ACCEPT_MODAL: 'accept-modal',
-  REJECTED_MODAL: 'rejected-modal',
-};
